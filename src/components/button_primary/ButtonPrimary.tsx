c409312511cbf4d11c178b58b50ffd885cdf2c42
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2024-09-24 13:30:18 -0400

    feat: add primary button component

--- src/components/button_primary/ButtonPrimary.tsx
+++ src/components/button_primary/ButtonPrimary.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import './ButtonPrimary.css'
+import './ButtonPrimary.css';
 
-const ButtonPrimary = () => {
+type ButtonProps = {
+  size?: 'small' | 'medium' | 'large';
+  color?: 'primary' | 'secondary';
+  label: string;
+  onClick: () => void;
+};
+
+const ButtonPrimary = ({
+  size = 'medium',
+  color = 'primary',
+  label,
+  onClick,
+}: ButtonProps) => {
   return (
-    <>
-    </>
-  )
-}
+    <button className={`button ${size} ${color}`} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
 
-export default ButtonPrimary
+export default ButtonPrimary;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,21 @@
-import './Home.css'
+import './Home.css';
 
 // Importaciones de barril
-import { useExampleHook, useAnotherHook } from '../../hooks'
-import {getAllUsersParams} from '../../services';
+import { useExampleHook, useAnotherHook } from '../../hooks';
+import { getAllUsersParams } from '../../services';
 import { useEffect } from 'react';
-import { strings } from '../../utils'
-import { ChatModal, LoadingSpinner } from '../../components';
-import { SearchService, OfferService, ChatList, Candidates, CandidateProfile } from '../../pages';
+import { strings } from '../../utils';
+import { ButtonPrimary, ChatModal, LoadingSpinner } from '../../components';
+import {
+  SearchService,
+  OfferService,
+  ChatList,
+  Candidates,
+  CandidateProfile,
+} from '../../pages';
 // import { User } from '../../interfaces'
 
-
 const Home = () => {
-  
   // uso de hooks para evitar never used errors
   useExampleHook();
   useAnotherHook();
@@ -22,25 +26,30 @@ const Home = () => {
       age: 25,
       pet: true,
     })
-    .then((response) => {
-      console.log('Usuarios filtrados:', response.data);
-    })
-    .catch((error) => {
-      console.log('Error al obtener usuarios:', error);
-    });
+      .then((response) => {
+        console.log('Usuarios filtrados:', response.data);
+      })
+      .catch((error) => {
+        console.log('Error al obtener usuarios:', error);
+      });
   }, []);
-   
 
   return (
     <div>
-        <h1>{strings.welcome}</h1>
-        <h2>Home test 2</h2>
-        <ChatModal/>
-        <SearchService/>
-        <CandidateProfile/>
-        <LoadingSpinner/>
+      <h1>{strings.welcome}</h1>
+      <h2>Home test 2</h2>
+      <ChatModal />
+      <SearchService />
+      <CandidateProfile />
+      <LoadingSpinner />
+      <ButtonPrimary
+        size="small"
+        color="secondary"
+        label="Button"
+        onClick={() => console.log('Click here')}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
